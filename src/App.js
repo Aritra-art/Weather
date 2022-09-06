dcb50f9b12c0ab0bd69a9166a02c7f41479537ca
aritra <aritrachowdhury65@gmail.com>
2022-09-06 18:04:53 +0530

    UI Updating

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import coldBg from "./assets/winter.jpg";
+import hotBg from "./assets/sunny.jpg";
+
 function App() {
-  return <div>Weather</div>;
+  return (
+    <div className="app" style={{ backgroundImage: `url(${coldBg})` }}>
+      <div className="overlay">
+        <div className="container">
+          <div className="section section__inputs">
+            <input type="text" name="city" placeholder="enter city name ..." />
+            <button>°F</button>
+          </div>
+
+          <div className="section section__temperature">
+            <div className="icon">
+              <h3>London, GB</h3>
+              <img src="" alt="weatherIcon" />
+              <h3>Cloudy</h3>
+            </div>
+            <div className="temperature">
+              <h1>34 °C</h1>
+            </div>
+          </div>
+          {/* bottom description */}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
